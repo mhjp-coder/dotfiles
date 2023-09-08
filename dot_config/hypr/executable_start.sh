be0bd513124b4366c19cbd4a92085172ee71fb49
Mark Parsons <mark@mhjp.ca>
2023-09-08 09:54:48 -0600

    Update .config/hypr/start.sh

--- dot_config/hypr/executable_start.sh
+++ dot_config/hypr/executable_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-hyprctl dispatch exec [workspace 1 silent] firefox
-hyprctl dispatch exec [workspace 3 silent] code
-hyprctl dispatch exec [workspace 5 silent] discord
-hyprctl dispatch exec [workspace 2 silent] alacritty
-hyprctl dispatch exec [workspace 4 silent] youtube-music
+hyprctl dispatch exec workspace 1 silent firefox
+hyprctl dispatch exec workspace 3 silent code
+hyprctl dispatch exec workspace 5 silent discord
+hyprctl dispatch exec workspace 2 silent alacritty
+hyprctl dispatch exec workspace 4 silent youtube-music
 #hyprctl dispatch exec [workspace 6 silent]
