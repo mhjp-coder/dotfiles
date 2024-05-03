d96ac02c4eef87bcdbb8a5a5c78106a765b11808
Mark P <mark@mhjp.ca>
2024-05-03 16:38:05 -0600

    Update package list and install packages from pkglist.txt

--- dot_config/scripts/executable_bootstrap.sh
+++
@@ -1,6 +0,0 @@
-
-# enable, start, configure tuned
-sudo systemctl enable --now tuned
-# get current profile: tuned-adm active
-# options are ouput of: tuned-adm list
-sudo tuned-adm profile virtual-host
