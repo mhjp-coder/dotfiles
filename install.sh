b84df9a6d353443daa647b19ccbcfba5348b70c6
Mark Parsons <mark@mhjp.ca>
2024-10-16 15:27:57 -0600

    Refactor package installation script to update mirrorlist with rate-mirrors command and add zsh-fast-syntax-highlighting-git

--- install.sh
+++ install.sh
@@ -6,33 +6,82 @@
 ##########################################
 set -e
 
-# Setup pacman with color and parallel downloads
-sudo sed -i 's/#Color/Color/' /etc/pacman.conf
-sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 10/' /etc/pacman.conf
-
-# Add user to sudoers NOPASSWD
-sudo echo "$(whoami) ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/00_$(whoami)
-
-# Install base-devel, chezmoi, git, rust
-sudo pacman -S --noconfirm --needed base-devel chezmoi git rust zsh
-
-sudo chsh -s $(which zsh) $(whoami)
-#
-# Install AUR helper paru
-if ! command -v paru 2>&1 >/dev/null
-then
-    echo "########### Installing paru AUR helper ###########"
-    rm -rf paru
-    git clone https://aur.archlinux.org/paru.git
-    cd paru
-    makepkg --syncdeps --install --noconfirm --needed
-    cd ..
-    rm -rf paru
+# Check if the script is being run as root
+if [ "$EUID" -e 0 ]; then
+    echo "Please do nut run this script as root."
+    exit 1
 fi
 
-# Update mirrors
-paru -S --noconfirm --needed rate-mirrors-bin
-rate-mirrors --country CA,US arch | sudo tee /etc/pacman.d/mirrorlist
+# Prompt the user to enter a username
+read -p "Please Enter the GitHub username to get the dot files from: " git_hub_username
 
-# Get the dotfiles
-chezmoi init --apply mhjp-coder
+# Get the current user
+who_am_i=$(whoami)
+
+if [[ "$(uname)" == "Linux" ]]; then
+    DISTRO=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
+    echo "This is a Linux system running $DISTRO."
+    if [[ "$DISTRO" == "arch" ]]; then
+        echo "##########   Setting up Pacman with color and parallel downloads   ##########"
+        pacman_setup
+        echo "##########   Installing packages needed for setup   ##########"
+        install_needed_pkgs
+        echo "##########   Adding user to sudoers, no password needed   ##########"
+        add_user_to_sudoers
+        echo "##########   Finding fastest mirrors   ##########"
+        update_mirrorlist
+        echo "##########   Setting default shell to zsh   ##########"
+        set_default_shell
+
+    else
+        echo "This script is intended for Arch Linux."
+        exit 1
+    fi
+elif [[ "$(uname)" == "Darwin" ]]; then
+    echo "This is a macOS system. Skipping linux setup."
+else
+    echo "Unknown system."
+fi
+
+# Setup pacman with color and parallel downloads
+pacman_setup() {
+    sudo sed -i 's/#Color/Color/' /etc/pacman.conf
+    sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 10/' /etc/pacman.conf
+}
+
+# Install packages needed to setup the system
+install_needed_pkgs() {
+    sudo pacman -S --noconfirm --needed base-devel chezmoi git rust zsh
+}
+
+# Add user to sudoers NOPASSWD
+add_user_to_sudoers() {
+    sudo echo "$who_am_i ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/00_$who_am_i
+}
+
+# Install AUR helper paru
+install_paru() {
+    if ! command -v paru 2>&1 >/dev/null
+    then
+        echo "########### Installing paru AUR helper ###########"
+        rm -rf paru
+        git clone https://aur.archlinux.org/paru.git
+        cd paru
+        makepkg --syncdeps --install --noconfirm --needed
+        cd ..
+        rm -rf paru
+    fi
+}
+
+# Install rate-mirrors and update the mirrorlist
+update_mirrorlist() {
+    paru -S --noconfirm --needed rate-mirrors-bin
+    rate-mirrors --country CA,US arch | sudo tee /etc/pacman.d/mirrorlist
+}
+
+set_default_shell() {
+    sudo chsh -s $(which zsh) $who_am_i
+}
+
+# Get the dotfiles and apply them
+chezmoi init --apply $git_hub_username
