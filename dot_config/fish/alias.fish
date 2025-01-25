077ef808356509611f87ee5ed9aeffb30e11fd2f
work <mark@mhjp.ca>
2025-01-24 22:43:02 -0700

    Update .config/fish/alias.fish

--- dot_config/fish/alias.fish
+++ dot_config/fish/alias.fish
@@ -7,48 +7,36 @@
     alias lt 'eza --tree $eza_params --level 3' --save
     alias tree 'eza --tree $eza_params --level 3' --save
 
-    # Git
-    alias gs        'git'
-
-
-    # ip
-    alias showip    'ip -br -c addr show'
-    alias ip        'ip -c'
-
-    # Python
-    alias uvi       'uv init'
-    alias uvs       'uv sync'
-    alias uva       'uv add'
-    alias uvr       'uv remove'
-
-    # Misc
-    alias cat       'bat'
-    alias zd        'z $HOME/Developer'
-    alias sc        '$HOME/.config/fish/config.fish | source'
-    alias tarnow    'tar -acf '
-    alias untar     'tar -zxvf '
-    alias fixpacman 'sudo rm /var/lib/pacman/db.lck'
-    alias rmpkg     'paru -Rcns '
-    alias psmem     'ps auxf | sort -nr -k 4'
-    alias psmem10   'ps auxf | sort -nr -k 4 | head -10'
-    alias grep      'grep --color=auto'
-    alias fgrep     'grep -F --color=auto'
-    alias egrep     'grep -E --color=auto'
-    alias hw        'hwinfo --short'
-
-    # Chezmoi
-    alias cz        'chezmoi '
-    alias czcd      'chezmoi cd'
-    alias cza       'chezmoi add '
-    alias czra      'chezmoi re-add '
-    alias czu       'chezmoi update'
-    alias czap      'chezmoi apply'
-    alias cze       'chezmoi edit --apply '
-    alias czs       'chezmoi status'
-    alias czd       'chezmoi doctor'
-    alias czf       'chezmoi forget '
-
-    # Vim
-    alias vim       'nvim'
-    alias vi        'nvim'
-    alias v         'nvim'
+    alias gs 'git' --save
+    alias showip 'ip -br -c addr show' --save
+    alias ip 'ip -c addr show'  --save
+    alias uvi 'uv init' --save
+    alias uvs 'uv sync' --save
+    alias uva 'uv add' --save
+    alias uvr 'uv remove' --save
+    alias cat 'bat' --save
+    alias zd 'z $HOME/Developer' --save
+    alias sc '$HOME/.config/fish/config.fish | source' --save
+    alias tarnow 'tar -acf ' --save
+    alias untar 'tar -zxvf ' --save
+    alias fixpacman 'sudo rm /var/lib/pacman/db.lck' --save
+    alias rmpkg 'paru -Rcns ' --save
+    alias psmem 'ps auxf | sort -nr -k 4' --save
+    alias psmem10 'ps auxf | sort -nr -k 4 | head -10' --save
+    alias grep 'grep --color=auto' --save
+    alias fgrep 'grep -F --color=auto' --save
+    alias egrep 'grep -E --color=auto' --save
+    alias hw 'hwinfo --short' --save
+    alias cz 'chezmoi ' --save
+    alias czcd 'chezmoi cd' --save
+    alias czra 'chezmoi re-add ' --save
+    alias czap 'chezmoi apply' --save
+    alias cza 'chezmoi add ' --save
+    alias czu 'chezmoi update' --save
+    alias cze 'chezmoi edit --apply ' --save
+    alias czs 'chezmoi status' --save
+    alias czd 'chezmoi doctor' --save
+    alias czf 'chezmoi forget ' --save
+    alias vim 'nvim' --save
+    alias vi 'nvim' --save
+    alias v 'nvim' --save
